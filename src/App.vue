46e3c660a35c794ad7d908f9cbe712d6ab43cb99
AsenCME <asen.connor@abv.bg>
2018-12-29 23:37:56 +0200

    finished big home

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+export default {
+  created() {
+    document.documentElement.style.fontSize = "18px";
+    if (this.isMobile()) document.documentElement.style.fontSize = "30px";
   }
+};
+</script>
+
+<style lang="scss">
+html,
+body,
+#app {
+  margin: 0px;
+  padding: 0px;
+  font-family: "Montserrat", sans-serif;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- Mobile Version -->
+    <div v-if="isMobile()">nigga we mobile</div>
+
+    <!-- Desktop & Tablet Version -->
+    <div
+      v-else
+      class="desktop"
+    >
+
+      <div class="text">
+        <div class="one">Енерджи</div>
+        <div class="two">Про-Консулт</div>
+        <div class="text-line"></div>
+        <div class="motto">Винаги може повече <span class="dot">.</span></div>
+      </div>
+
+      <div class="circuit">
+
+        <div class="circles">
+          <span class="tl"></span>
+          <span class="tr"></span>
+          <span class="bl"></span>
+          <span class="br"></span>
+          <span class="lmt"></span>
+          <span class="lmb"></span>
+        </div>
+
+        <div class="blanks">
+          <span
+            class="switch"
+            @click="click"
+          ></span>
+          <span class="lamp"></span>
+          <span class="battery"></span>
+          <span class="bulb"></span>
+        </div>
+
+        <div class="things">
+          <div
+            class="label"
+            @click="click"
+          >BEGIN</div>
+          <span
+            class="switch"
+            @click="click"
+          ></span>
+          <span class="battery">
+            <div class="top"></div>
+            <div class="bottom"></div>
+          </span>
+        </div>
+
+        <!-- <img
+          class="circuit-img"
+          src="./../assets/circuit.svg"
+          alt=""
+        /> -->
+
+        <img
+          src="./../assets/bulb.svg"
+          alt=""
+          class="bulb"
+        />
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import "@/styles/home.scss";
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  methods: {
+    click() {
+      // go to hub
+      document.querySelector(".things .switch").classList.add("clicked");
+      console.log("clickity click");
+    }
   }
 };
 </script>
+
